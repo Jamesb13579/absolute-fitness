e15454a8befccc79e23cbe2996b2b803e653f153
Jamesb13579 <jamesbagge@ymail.com>
2022-11-07 23:45:14 +0000

    added product.html and functionality

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def products(request):
+    """ a view to return shop page"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
