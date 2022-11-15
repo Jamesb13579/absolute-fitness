7864c09b821a957a41be7c4e5b340917ad437e29
Jamesb13579 <jamesbagge@ymail.com>
2022-11-15 00:31:07 +0000

    added to my stripe setup

--- absolute_fitness/settings.py
+++ absolute_fitness/settings.py
@@ -176,3 +176,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STANDARD_DELIVERY_COST = 5
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
