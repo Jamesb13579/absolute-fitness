addd4af5616f6e0b02946b371f6baa8ecfc0fd25
Jamesb13579 <jamesbagge@ymail.com>
2022-11-19 05:28:12 +0000

    changed secret key and set debug

--- absolute_fitness/settings.py
+++ absolute_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(&p5%gh(i*wff+3=$x=iw6ob=u1&_(k2n5&8(zfq)d$-2)7qoe'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['absolute-fitness.herokuapp.com', 'localhost']
 
