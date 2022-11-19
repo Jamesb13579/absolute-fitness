b19abc32fb13bd934d1321aa77ea5586f7fa3c49
Jamesb13579 <jamesbagge@ymail.com>
2022-11-19 06:34:38 +0000

    added aws media and static files to settings

--- absolute_fitness/settings.py
+++ absolute_fitness/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'profiles',
 
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -182,6 +183,24 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'USE_AWS' in os.environ:
+    #bucket config
+    AWS_STORAGE_BUCKET_NAME = 'absolutefitness'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = 'os.environ.get(AWS_ACCESS_KEY_ID)'
+    AWS_SECRET_ACCESS_KEY = 'os.environ.get(AWS_SECRET_ACCESS_KEY)'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILES_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # over ride media and static urls in production
+    STATIC_URL =f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL =f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIaFILES_LOCATION}/'
 
 STANDARD_DELIVERY_COST = 5
 STRIPE_CURRENCY = 'usd'
