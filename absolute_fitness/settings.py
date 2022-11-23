d1a87ca5e70f12f2a25f8eb13f1f21ffc4502b1d
Jamesb13579 <jamesbagge@ymail.com>
2022-11-23 03:55:45 +0000

    static files

--- absolute_fitness/settings.py
+++ absolute_fitness/settings.py
@@ -188,19 +188,20 @@ if 'USE_AWS' in os.environ:
     #bucket config
     AWS_STORAGE_BUCKET_NAME = 'absolutefitness'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = 'os.environ.get(AWS_ACCESS_KEY_ID)'
-    AWS_SECRET_ACCESS_KEY = 'os.environ.get(AWS_SECRET_ACCESS_KEY)'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # static and media files
+    # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILES_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # over ride media and static urls in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 STANDARD_DELIVERY_COST = 5
 STRIPE_CURRENCY = 'usd'
